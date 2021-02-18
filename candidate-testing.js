6709ca24d3f4922dd65d30f63be35ea67728a0f7
chrissyschepker <chrissyschepker@yahoo.com>
2021-02-18 17:08:00 +0000

    Trailing Spaces Issue

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,10 +8,10 @@ let candidateName = " ";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
-let questions = ["\n1) Who was the first American woman in space? ", "\n2) True or False: 5000 meters = 5 kilometers. ", 
-"\n3) (5+3) / 2*10 =", 
+let questions = ['\n1) Who was the first American woman in space? ', '\n2) True or False: 5000 meters = 5 kilometers. ', 
+'\n3) (5+3) / 2*10 =', 
 "\n4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
-"\n5) What is the minimum crew size for the International Space Station(ISS)? "];
+'\n5) What is the minimum crew size for the International Space Station(ISS)? '];
 
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3" ];
 let candidateAnswers = [];
@@ -21,7 +21,8 @@ function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question('What is your name?:');
 candidateName = String(candidateName);
-console.log(`Candidate Name: ${candidateName}`);
+console.log(`Hello ${candidateName}!`)
+//console.log(`Candidate Name: ${candidateName}`);
 }
 
 
@@ -39,6 +40,8 @@ i++;
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  console.log(`Candidate Name: ${candidateName}`);
+  
   candidateScore = 0;
   maxScore = 5;
   for (let i = 0; i < correctAnswers.length; i++) {
